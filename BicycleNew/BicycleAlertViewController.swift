9397e6717e64fbd168eb32f8142a1befd9caf6d1
k920049 <k920049@naver.com>
2016-12-12 18:09:09 +0900

    Start collaborating

---
+++ BicycleNew/BicycleAlertViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BicycleAlertViewController.swift
+//  BicycleNew
+//
+//  Created by JeaSung Park on 2016. 12. 12..
+//  Copyright © 2016년 JeaSung Park. All rights reserved.
+//
+
+import Foundation
---
+++ BicycleNew/BicycleRecommendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BicycleRecommendTableViewCell.swift
+//  BicycleNew
+//
+//  Created by JeaSung Park on 2016. 12. 12..
+//  Copyright © 2016년 JeaSung Park. All rights reserved.
+//
+
+import UIKit
+
+class BicycleRecommendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
