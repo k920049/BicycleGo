c999dba79b535653a318f48771050989735d7817
k920049 <k920049@naver.com>
2016-12-17 18:53:57 +0900

    Added tableView for certification

---
+++ BicycleNew/UICertificationTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  UICertificationTableViewCell.swift
+//  BicycleNew
+//
+//  Created by JeaSung Park on 2016. 12. 17..
+//  Copyright © 2016년 JeaSung Park. All rights reserved.
+//
+
+import UIKit
+
+class UICertificationTableViewCell: UITableViewCell {
+    @IBOutlet var labelName : UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BicycleNew/UICertificationTableViewController.swift
+++ BicycleNew/UICertificationTableViewController.swift
@@ -32,23 +32,28 @@ class UICertificationTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showCamera", sender: self)
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "certCell", for: indexPath) as! UICertificationTableViewCell
 
         // Configure the cell...
+        cell.labelName.text = "What you want?"
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
